ea4a39d705cb4ad4d6246dbd195bea49cf56d86a
MarcelShin <marcelovieira048@gmail.com>
2025-10-18 11:45:43 -0300

    feat: login page completed

--- src/app/components/default-login-layout/default-login-layout.ts
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -12,6 +12,7 @@ export class DefaultLoginLayout {
   @Input() title: string ="";
   @Input() primaryBtnText: string ="";
   @Input() secondaryBtnText: string ="";
+  @Input() disablePrimaryBtn: boolean = true;
 
   @Output("submit") onSubmit = new EventEmitter();
   @Output("navigate") onNavigate = new EventEmitter();

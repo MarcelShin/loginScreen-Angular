91e6549dacbb6e1ad2db1410f2151ee6f1a26de5
MarcelShin <marcelovieira048@gmail.com>
2025-10-18 11:38:51 -0300

    fix: ngx-toastr error

--- src/app/components/default-login-layout/default-login-layout.ts
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -12,9 +12,15 @@ export class DefaultLoginLayout {
   @Input() title: string ="";
   @Input() primaryBtnText: string ="";
   @Input() secondaryBtnText: string ="";
+
   @Output("submit") onSubmit = new EventEmitter();
+  @Output("navigate") onNavigate = new EventEmitter();
 
   submit(){
     this.onSubmit.emit();
+  }
+
+  navigate(){
+    this.onNavigate.emit();
   }
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,8 +1,11 @@
+import { LoginService } from './../../services/login.service';
 import { Component } from '@angular/core';
 import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInput } from "../../components/primary-input/primary-input";
 import { NgOptimizedImage } from "@angular/common";
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 interface LoginForm {
   email: FormControl,
@@ -18,13 +21,20 @@ interface LoginForm {
     PrimaryInput,
     NgOptimizedImage
 ],
+providers: [
+  LoginService
+],
   templateUrl: './login.html',
   styleUrls: ['./login.scss']
 })
 export class Login {
   loginForm!: FormGroup<LoginForm>;
 
-  constructor(){
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
@@ -32,6 +42,13 @@ export class Login {
   }
 
   submit(){
-    console.log(this.loginForm.value)
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login successfully!"),
+      error: () => this.toastService.error("Unexpected error, please try again later!")
+    })
+  }
+
+  navigate(){
+    this.router.navigate(["signup"])
   }
 }
